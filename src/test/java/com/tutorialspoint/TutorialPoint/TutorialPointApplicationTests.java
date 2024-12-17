175c6343e9e0a4dc95f40498dee0596e3e5939c3
Phenny <mwaisakaphenny@gmail.com>
2024-12-17 20:36:47 +0300

    Initial commit

---
+++ src/test/java/com/tutorialspoint/TutorialPoint/TutorialPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorialspoint.TutorialPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
